b0cbc2e20f91b7b80c08eaa9f114c796fdff93a4
Dmitry Rodionov <dmitry.rodionov@gmail.com>
2017-05-30 11:01:52 +0300

    Email support

--- index.js
+++ index.js
@@ -19,7 +19,18 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  emailAdapter: {
+    module: 'parse-server-simple-mailgun-adapter',
+    options: {
+      // The address that your emails come from
+      fromAddress: process.env.MAILGUN_FROM,
+      // Your domain from mailgun.com
+      domain: process.env.MAILGUN_DOMAIN,
+      // Your API key from mailgun.com
+      apiKey: process.env.MAILGUN_API,
+    }
+  },
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
