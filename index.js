8c75aece438988c0c25e5f514b91d5c6d1dc8f19
Dmitry Rodionov <dmitry.rodionov@gmail.com>
2017-06-01 10:56:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ var api = new ParseServer({
   fileKey: process.env.FILE_KEY || 'myFileKey',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appName: process.env.APP_NAME,
+  publicServerURL: process.env.SERVER_URL,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
